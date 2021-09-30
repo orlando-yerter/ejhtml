bc63b89627c3f605f644e77fd9d41be32f4d9a93
orlando-yerter <53948381+orlando-yerter@users.noreply.github.com>
2021-09-29 23:11:01 -0300

    agregado js,yformulario2

---
+++ js/formulario.js
@@ -0,0 +1,19 @@
+function validar(){
+    var form =document.form;
+      if(form.nombre.value==0){
+        alert("el nombre esta vacio");
+        form.nombre.value= "";
+        form.nombre.focus();
+        return  false;
+      }
+      if(form.email.value==0){
+        alert("el mail  esta vacio");
+        form.email.value= "";
+        form.email.focus();
+        return  false;
+    }
+   document.write("los datos se han enviados correctamente");
+   form.submit();
+ 
+
+}
